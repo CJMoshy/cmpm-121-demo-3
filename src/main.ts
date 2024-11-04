2c416d08a8b1ca12d1617d65ea47406fb32b6590
= <cjmoshy12@gmail.com>
2024-11-04 08:31:29 -0800

    Initial commit to test github actions.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
 // todo
+import "./style.css";
+
+const helloWorldBtn = document.createElement("button");
+helloWorldBtn.textContent = "Click Me!";
+helloWorldBtn.addEventListener("click", () => {
+  alert("You clicked the button!");
+});
+document.getElementById("app")?.append(helloWorldBtn);
