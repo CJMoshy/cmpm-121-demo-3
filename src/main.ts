61fdedd733476c09e5af126a384abe1c2bcdec83
= <cjmoshy12@gmail.com>
2024-11-05 17:43:57 -0800

    Map working

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,36 @@
 // todo
+// @deno-types="npm:@types/leaflet@^1.9.14"
+
+import leaflet from "leaflet";
+
+import "leaflet/dist/leaflet.css";
 import "./style.css";
 
-const helloWorldBtn = document.createElement("button");
-helloWorldBtn.textContent = "Click Me!";
-helloWorldBtn.addEventListener("click", () => {
-  alert("You clicked the button!");
+import "./leafletWorkaround.ts";
+
+// define list of spawn points for player
+const spawnLocations = {
+  OAKES_CLASSROOM: leaflet.latLng(36.98949379578401, -122.06277128548504),
+};
+
+// Tunable gameplay parameters
+const GAMEPLAY_ZOOM_LEVEL = 19;
+
+// create the map with leaflet
+const map = leaflet.map(document.getElementById("map")!, {
+  center: spawnLocations.OAKES_CLASSROOM,
+  zoom: GAMEPLAY_ZOOM_LEVEL,
+  minZoom: GAMEPLAY_ZOOM_LEVEL,
+  maxZoom: GAMEPLAY_ZOOM_LEVEL,
+  zoomControl: false,
+  scrollWheelZoom: false,
 });
-document.getElementById("app")?.append(helloWorldBtn);
+
+// Populate the map with a background tile layer
+leaflet
+  .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  })
+  .addTo(map);
