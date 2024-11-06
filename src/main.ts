7ed774008885424d2fb7f41a37b4f9d01c56b7eb
= <cjmoshy12@gmail.com>
2024-11-05 17:45:03 -0800

    added in the player.

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ import "./style.css";
 
 import "./leafletWorkaround.ts";
 
+// import luck from "./luck.ts";
+
 // define list of spawn points for player
 const spawnLocations = {
   OAKES_CLASSROOM: leaflet.latLng(36.98949379578401, -122.06277128548504),
@@ -15,6 +17,9 @@ const spawnLocations = {
 
 // Tunable gameplay parameters
 const GAMEPLAY_ZOOM_LEVEL = 19;
+// const TILE_DEGREES = 1e-4;
+// const NEIGHBORHOOD_SIZE = 8;
+// const CACHE_SPAWN_PROBABILITY = 0.1;
 
 // create the map with leaflet
 const map = leaflet.map(document.getElementById("map")!, {
@@ -34,3 +39,21 @@ leaflet
       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   })
   .addTo(map);
+
+const player = {
+  coins: 0,
+  marker: leaflet.marker(spawnLocations.OAKES_CLASSROOM),
+};
+
+const statusPanel = document.querySelector<HTMLDivElement>("#coins")!; // element `statusPanel` is defined in index.html
+statusPanel.innerHTML = "0...";
+
+function main() {
+  // here is where we would load stuff from local storage
+
+  //  then we can set player and go!
+  player.marker.bindTooltip("You are here!");
+  player.marker.addTo(map);
+}
+
+main();
